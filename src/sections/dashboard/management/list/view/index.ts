154920e495cc29346bbc892b7e03be186e59caf5
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-09-10 12:45:12 +0300

    remove unused files

--- src/sections/dashboard/management/list/view/index.ts
+++ src/sections/dashboard/management/list/view/index.ts
@@ -1,3 +1,1 @@
-export { default as EditView } from './user-edit-view';
 export { default as ListView } from './user-list-view';
-export { default as ProfileView } from "./user-profile-view";
---
+++ src/sections/user/view/index.ts
@@ -0,0 +1,3 @@
+export { default as EditView } from './user-edit-view';
+export { default as ListView } from './user-list-view';
+export { default as ProfileView } from "./user-profile-view";
